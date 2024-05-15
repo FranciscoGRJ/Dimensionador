2928a815ae1930d34444b8041b5b5f73ab02edb7
Jose-sudo-star <jfranciscogromero@gmail.com>
2024-05-15 00:14:06 -0600

    New Calibrations

--- dimensionador.py
+++ dimensionador.py
@@ -3,12 +3,16 @@ import cv2
 import numpy as np
 import utils
 
-webcam = False
+webcam = True
 path='1.jpg'
 cap = cv2.VideoCapture(0)
 cap.set(10,160) #Set property 10 (brightness)
 cap.set(3,1920) #Set property 3 (frame width)
 cap.set(4,1080) #Set porperty 4 (frame lenght)
+
+scaleFactor = 3
+referenceWidth = 210*scaleFactor
+referenceHeigth = 297*scaleFactor
 
 while True:
     if webcam : 
@@ -17,17 +21,36 @@ while True:
         img = cv2.imread(path)
 
     img, contours = utils.getContours(img,
-                                           cannyThreshold=[150,150], #Threshold for certanty
-                                           showCanny=True,           #Show Canny
-                                           draw=True,                #Draw contours
                                            minArea=50000,            #Minimum Area to be detected
                                            filter=4)                 #Filter based on number of corners
     if len(contours) != 0:
         biggest = contours[0][2]
-        print(biggest)
+        #print(biggest)
+        imgWarp = utils.warpImage (img,biggest,referenceWidth,referenceHeigth)
+        #cv2.imshow('A4', imgWarp)
+        
+        img2, contours2 = utils.getContours(imgWarp,
+                                           cannyThreshold=[50,50], #Threshold for certanty
+                                           draw=False,                #Draw contours
+                                           minArea=2000,            #Minimum Area to be detected
+                                           filter=4)                 #Filter based on number of corners
+        if len(contours) != 0:
+            for obj in contours2:
+                cv2.polylines(img2,[obj[2]],True,(0,255,0),2)
+                newPoint = utils.reorder(obj[2])
+                newWidth = round((utils.findDis(newPoint[0][0]//scaleFactor,newPoint[1][0]//scaleFactor)/10),1)
+                newHeigth = round((utils.findDis(newPoint[0][0]//scaleFactor,newPoint[2][0]//scaleFactor)/10),1)
+                
+                cv2.arrowedLine(img2,(newPoint[0][0][0], newPoint[0][0][1]),(newPoint[1][0][0],newPoint[1][0][1]), (255,0,255),3,8,0,0.05)
+                cv2.arrowedLine(img2,(newPoint[0][0][0], newPoint[0][0][1]),(newPoint[2][0][0],newPoint[2][0][1]), (255,0,255),3,8,0,0.05)
+                x,y,w,h = obj[3]
+                cv2.putText(img2, '{}cm'.format(newWidth), (x + 30, y - 10), cv2.FONT_HERSHEY_COMPLEX_SMALL, 1.5, (255,0,255),2)
+                cv2.putText(img2, '{}cm'.format(newHeigth), (x - 70, y + h // 2), cv2.FONT_HERSHEY_COMPLEX_SMALL, 1.5, (255,0,255),2)
+                
+        cv2.imshow('found',img2)
         
     img = cv2.resize(img,(0,0),None,0.5,0.5)
     cv2.imshow('original', img)
     cv2.waitKey(1)
 
-#test comment
+
--- utils.py
+++ utils.py
@@ -6,8 +6,8 @@ def getContours(img,cannyThreshold=[100,100],showCanny=False, minArea = 1000, fi
     imgBlurr = cv2.GaussianBlur(imgGrayScale,(5,5),1)
     imgCanny = cv2.Canny(imgBlurr,cannyThreshold[0],cannyThreshold[1])
     kernel =np.ones((5,5))
-    imgDialation =cv2.dilate(imgCanny,kernel,iterations=2)
-    imgThreshold= cv2.erode(imgDialation,kernel,iterations=1)
+    imgDialation =cv2.dilate(imgCanny,kernel,iterations=3)
+    imgThreshold= cv2.erode(imgDialation,kernel,iterations=2)
     if showCanny:
         cv2.imshow('canny',imgThreshold)
 
@@ -33,3 +33,30 @@ def getContours(img,cannyThreshold=[100,100],showCanny=False, minArea = 1000, fi
             cv2.drawContours(img,con[4],-1,(0,0,255),3)
     
     return img, finalContours
+
+def reorder (myPoints):
+    print(myPoints.shape)
+    myNewPoints=np.zeros_like(myPoints)
+    myPoints=myPoints.reshape((4,2))
+    add= myPoints.sum(1)
+    myNewPoints[0] = myPoints[np.argmin(add)]
+    myNewPoints[3] = myPoints[np.argmax(add)]
+    diff = np.diff(myPoints, axis = 1)
+    myNewPoints[1] =myPoints[np.argmin(diff)]
+    myNewPoints[2] =myPoints[np.argmax(diff)]
+    return myNewPoints
+
+def warpImage (img,points,width,heigth,pad = 20):
+    #print(points)
+    points = reorder(points)
+    point1 =np.float32(points)
+    point2 = np.float32([[0,0],[width,0],[0,heigth],[width,heigth]])
+    matrix =cv2.getPerspectiveTransform(point1,point2)
+    imageWrapped = cv2.warpPerspective(img,matrix,[width,heigth])
+    
+    imageWrapped = imageWrapped[pad:imageWrapped.shape[0]-pad, pad:imageWrapped.shape[1]-pad]
+    
+    return imageWrapped
+
+def findDis (point1, point2):
+    return ((point2[0]-point1[0])**2 + (point2[1]-point1[1])**2)**0.5
